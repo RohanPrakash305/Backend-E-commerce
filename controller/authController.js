78bab1f27ad39a4766a10893217a49b010408e0e
RohanPrakash305 <atulraj844114@gmail.com>
2025-08-04 13:02:27 +0530

    data not post in database

--- controller/authController.js
+++ controller/authController.js
@@ -12,13 +12,13 @@ export const registration = async  (req  , res) => {
         const existUser = await User.findOne({email})
 
         if(existUser){
-            return res.status(400).json({messagr:"User alrady exist"})
+            return res.status(400).json({message:"User alrady exist"})
         }
         if(!validator.isEmail(email)){
-            return res.status(400).json({messagr:"Enter vaalid Email"})
+            return res.status(400).json({message:"Enter vaalid Email"})
         }
         if(password.length<8){
-            return res.status(400).json({messagr:"Enter strong password"})
+            return res.status(400).json({message:"Enter strong password"})
         }
 
         let hashPassword = await bcrypt.hash(password,10)
@@ -46,11 +46,11 @@ export const login = async (req,res) =>{
         let{email,password} =req.body;
         let user = await User.findOne({email})
         if(!user){
-             return res.status(404).json({messagr:"User is not found"})
+             return res.status(404).json({message:"User is not found"})
         }
         let isMatch = await bcrypt.compare(password,user.password)
         if(!isMatch){
-              return res.status(404).json({messagr:"incorrect password"})
+              return res.status(404).json({message:"incorrect password"})
         }
         let token = await genToken(user._id)
         res.cookie("token",token,{
